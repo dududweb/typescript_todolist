ba8c847590e248242bbcb52bfd1e9901b3d41046
JooyoungHwnag <joodudsdx@gmail.com>
2022-04-10 19:45:31 +0900

    Fix: readme

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,15 +1,33 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
 
 const userSchema = mongoose.Schema({
   email: {
     type: String,
     trim: true,
     unique: 1,
+    required: true,
   },
   password: {
     type: String,
     minlength: 5,
+    required: true,
   },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 const User = mongoose.model("User", userSchema);
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const cofig = require("./config/key");
+const config = require("./config/key");
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
 app.use(express.urlencoded({ extended: true }));
 
 const { User } = require("./models/Users");
 
-const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
 app.get("/", (req, res) => {
@@ -17,19 +23,17 @@ app.get("/api/login", (req, res) => {
   res.send({ message: "login" });
 });
 
-app.post("/register", (req, res) => {
-  res.send("성공");
+app.post("/api/register", (req, res) => {
+  const user = new User({ email: "ev@@", password: "1234445" });
+  console.log("user>>>", user);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ message: "success" });
+  });
 });
 
 const port = 8080;
-
-var db;
-MongoClient.connect(cofig.mongoURI, function (에러, client) {
-  if (에러) return console.log(에러);
-
-  db = client.db("nodes");
-
-  app.listen(port, () => {
-    console.log(`server is running on ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
 });
